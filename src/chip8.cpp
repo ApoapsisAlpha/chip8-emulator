48495f2a349c89bdea1e3209b0e6bbc35e238b86
David Tsukernik <david.tsuki@gmail.com>
2020-07-25 16:58:36 -0400

    Fix opcode 0xC implementation

--- src/chip8.cpp
+++ src/chip8.cpp
@@ -13,5 +13,5 @@ void Chip8::init_opcodes() {
     opcode_table[0x9] = [this](uint16_t ins_data) { if (V[ins_data >> 8] != V[(ins_data & 0xF0) >> 4]) pc += 2; };
     opcode_table[0xA] = [this](uint16_t ins_data) { I = ins_data; };
     opcode_table[0xB] = [this](uint16_t ins_data) { pc = ins_data + V[0]; };
-    opcode_table[0xC] = [this](uint16_t ins_data) { V[ins_data >> 8] = rand() % 256; };
+    opcode_table[0xC] = [this](uint16_t ins_data) { V[ins_data >> 8] = (ins_data & 0xFF) & (rand() % 256); };
 }
